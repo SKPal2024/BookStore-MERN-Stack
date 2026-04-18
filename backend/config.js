93350b424daed86b91f1e41c37783e5584543e41
Surya Kanta Pal <skpeng2005@gmail.com>
2026-04-18 11:13:36 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  //'mongodb+srv://root:skp%252026@books-store-mern.c7ba8z1.mongodb.net/BookStore?retryWrites=true&w=majority';
+  'mongodb://Surya:mnbv2026@ac-yzgewp9-shard-00-00.ywuejys.mongodb.net:27017,ac-yzgewp9-shard-00-01.ywuejys.mongodb.net:27017,ac-yzgewp9-shard-00-02.ywuejys.mongodb.net:27017/BookStore?ssl=true&replicaSet=atlas-7mv2qn-shard-0&authSource=admin&retryWrites=true&w=majority';
+  // Please create a free database for yourself.
+// This database will be deleted after tutorial
